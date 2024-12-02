7e799be03d55b155209267bc49746df7df1a6c27
SergioS7568 <sergio7568@hotmail.com>
2024-12-01 23:01:03 -0300

    feat/ Header is done and with it's properties

--- src/Common/Header/Header.tsx
+++ src/Common/Header/Header.tsx
@@ -1,4 +1,18 @@
+import Grid from "../../Components/Grid/Grid";
+import DomicilioDigitalJustLogo from "../../assets/images/DomicilioDigitalJustLogo.jpeg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Grid container gap={1} alignContent="between">
+        <Grid item xs={6} xl={6}>
+          <img src={DomicilioDigitalJustLogo}></img>
+        </Grid>
+        <Grid item xs={6} xl={6}>
+          <p>Domicilio digital</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 export default Header;
