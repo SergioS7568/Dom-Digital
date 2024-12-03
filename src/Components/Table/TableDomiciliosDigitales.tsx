d7180b6a30f2a5726a981666ee5503b9dbed375e
SergioS7568 <sergio7568@hotmail.com>
2024-12-03 11:30:44 -0300

    feature/ A table and rows of said table were added

--- src/Components/Table/TableDomiciliosDigitales.tsx
+++ src/Components/Table/TableDomiciliosDigitales.tsx
@@ -1,34 +1,54 @@
+import RowDomiciliosDigitales from "./RowDomiciliosDigitales";
+interface UserProfile {
+  id: number;
+  name: string;
+
+  lastname: string;
+
+  profile: {
+    id: number;
+    name: string;
+  };
+}
+
 const TableDomiciliosDigitales = () => {
+  const userString = localStorage.getItem("usersDomDig");
+  const parsedData: UserProfile = userString ? JSON.parse(userString) : null;
+  console.log("Parsed Data:   ", parsedData);
+
+  const users: UserProfile[] =
+    parsedData && parsedData.content && Array.isArray(parsedData.content)
+      ? parsedData.data
+      : [];
+  console.log("users Data:   ", users);
+  if (!Array.isArray(users)) {
+    console.error("Users data is not an array:", users);
+    return (
+      <div>
+        <p>Error: Users data is not available or is in an invalid format.</p>
+      </div>
+    );
+  }
   return (
     <div className="overflow-x-auto">
       <table className="table">
         <thead>
           <tr>
-            <th></th>
+            <th>#</th>
             <td>Apellido</td>
             <td>Nombre</td>
             <td>Perfil</td>
           </tr>
         </thead>
         <tbody>
-          <tr className="hover">
-            <th>1</th>
-            <td>Test1</td>
-            <td>Test1</td>
-            <td>Test1</td>
-          </tr>
-          <tr className="hover">
-            <th>2</th>
-            <td>Test2</td>
-            <td>Test2</td>
-            <td>Test2</td>
-          </tr>
-          <tr className="hover">
-            <th>3</th>
-            <td>Test3</td>
-            <td>Test3</td>
-            <td>Test3</td>
-          </tr>
+          {users.map((user, index: number) => {
+            return (
+              <RowDomiciliosDigitales
+                users={user}
+                key={user.id}
+              ></RowDomiciliosDigitales>
+            );
+          })}
         </tbody>
       </table>
     </div>
--- src/View/RootingView.tsx
+++ src/View/RootingView.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import Footer from "../Common/Footer/Footer";
 import Header from "../Common/Header/Header";
+import { Api } from "../Components/api/Api";
+//import Apiedictos from "../Components/env/Apiedictos";
 import DomiciliosDigilatesView from "./DomiciliosDigilatesView";
 
 const RootingView = () => {
+  const [apiResult, setApiResult] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await Api("amin", "", "", "lastname:asc", "0,15");
+        setApiResult(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div
       className="bg-zinc-500   "
@@ -16,6 +33,11 @@ const RootingView = () => {
       <Header></Header>
       <DomiciliosDigilatesView></DomiciliosDigilatesView>
       <Footer></Footer>
+
+      <div>
+        <h2>API Test Result:</h2>
+        <p>{apiResult ? apiResult : "Loading..."}</p>
+      </div>
     </div>
   );
 };
